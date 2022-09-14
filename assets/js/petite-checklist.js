6dce1ed862eb5737070333dd5b6cddb18446fd74
rtorres <rene.torres@csps-efpc.gc.ca>
2022-09-14 13:52:16 -0400

    Initial Commit

---
+++ assets/js/petite-checklist.js
@@ -0,0 +1,40 @@
+import {
+    createApp,
+} from 'https://unpkg.com/petite-vue?module'
+const addCheckBox = (prop) => {
+    return {
+        $template: ('#petite_checklist'),
+        question: prop.question,
+        uniqueId: prop.uniqueId,
+        randNum: Math.floor(Math.random() * 6),
+    }
+}
+
+const textInputVal = (prop) => {
+    return {
+        $template: ('#petite_ta'),
+        question: prop.question,
+        isRequired: prop.isRequired,
+        personalThought: null,
+        isValid: null,
+        uniqueId: prop.uniqueId,
+        submitForm() {
+            if (this.personalThought === null || this.personalThought === '') {
+                if (this.isRequired !== true) {
+                    this.isValid = true
+                } else {
+                    this.isValid = false
+                }
+            } else {
+                this.isValid = true
+            }
+        }
+    }
+}
+
+createApp({
+    addCheckBox,
+    textInputVal
+}).mount()
+
+console.log('petite vue/checklist is ready')
